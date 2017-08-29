1de707d67e1ab96adfd589eb58f6742533af48dd
Mitch Hankins <mitchhankins92@gmail.com>
2017-08-29 14:22:23 -0600

    Implemented Decimal to Binary

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import { Dimensions, StyleSheet, View, Text } from 'react-native';
-import { Button, Card, Header, FormLabel, FormInput } from 'react-native-elements';
+import { Button, ButtonGroup, Card, Header, FormLabel, FormInput } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
+const convert = {
+  bin2dec : s => parseInt(s, 2).toString(10),
+  bin2hex : s => parseInt(s, 2).toString(16),
+  dec2bin : s => parseInt(s, 10).toString(2),
+  dec2hex : s => parseInt(s, 10).toString(16),
+  hex2bin : s => parseInt(s, 16).toString(2),
+  hex2dec : s => parseInt(s, 16).toString(10)
+};
 
 export default class App extends React.Component {
-  state = { input: '', conversion: 'Empty' }
+  constructor () {
+    super()
+    this.updateIndex = this.updateIndex.bind(this)
+  }
+
+  updateIndex (selectedIndex) {
+    this.setState({selectedIndex})
+  }
+
+  state = { input: '', output: 'Empty', selectedIndex: 2 }
 
   onButtonPress = () => {
-    this.setState({ conversion: this.state.input + 'asdff' })
+    result = convert.dec2bin(this.state.input);
+
+    this.setState({ output: result.split(/(.{4})/g).join(' ') })
+  }
+
+  onClearPress = () => {
+    this.setState({ output: '', input: '' })
   }
 
   render() {
+    const buttons = ['Dec2Bin', 'Bin2Dec', 'Dec2Hex', 'Dec2Oct']
+    const { selectedIndex } = this.state
+
     return (
       <View style={{ flex: 1 }}>
         <Header
@@ -23,6 +49,12 @@ export default class App extends React.Component {
         />
 
         <View style={{ paddingTop: 64 }}>
+          <ButtonGroup
+            onPress={this.updateIndex}
+            selectedIndex={selectedIndex}
+            buttons={buttons}
+            containerStyle={{height: 50}} />
+
           <Card title='Input'>
             <FormLabel style={{alignSelf: 'center' }}>
               Quantity to Convert
@@ -34,7 +66,7 @@ export default class App extends React.Component {
 
           <Card title='Output'>
             <Text style={{marginBottom: 10, alignSelf: 'center' }}>
-              <Text>{this.state.conversion}</Text>
+              <Text>{this.state.output}</Text>
             </Text>
           </Card>
         </View>
@@ -44,6 +76,14 @@ export default class App extends React.Component {
             raised
             iconRight
             onPress={this.onButtonPress}
+            icon={{name: 'clear'}}
+            backgroundColor="#c0392b"
+            title='Clear' />
+          <Button
+            style={{ paddingTop: 15 }}
+            raised
+            iconRight
+            onPress={this.onClearPress}
             icon={{name: 'cached'}}
             backgroundColor="#03A9F4"
             title='Convert' />
